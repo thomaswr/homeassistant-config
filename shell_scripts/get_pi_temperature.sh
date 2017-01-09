327a1d6215f42e353b2a144adfc053f9c3308379
thomaswr <thomas.w.riener@gmail.com>
2017-01-10 00:10:55 +0100

    Trial and error if the /sys path is not working...

--- shell_scripts/get_pi_temperature.sh
+++ shell_scripts/get_pi_temperature.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo $(bc <<<"scale=2;$((`cat /sys/class/thermal/thermal_zone0/temp`))/1000")
+cat /sys/class/thermal/thermal_zone0/temp
+#echo $(bc <<<"scale=2;$((`cat /sys/class/thermal/thermal_zone0/temp`))/1000")
